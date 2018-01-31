d2b77c691d10c1c39cfb50a520e8ba4f2c1e7148
Martine Verburgh <martineverburgh@gmail.com>
2018-01-31 20:43:26 +0100

    sellProduct() toegevoegd.

--- GifKikkerWebshop/ProductFactory.java
+++ GifKikkerWebshop/ProductFactory.java
@@ -1,24 +1,24 @@
 public class ProductFactory {
-    
+
     private Beer gifKikker, heineken, grolsch, hertogJan;
     private Stock stock;
-    
+
     public ProductFactory() {
         stock = new Stock();        
     }
-    
+
     public void startWebShop() {
         createBeer();
         createProducts();
     }
-    
+
     private void createBeer() {
         gifKikker = new Beer(7.5);
         heineken = new Beer(5.0);
         grolsch = new Beer(5.0);
         hertogJan = new Beer(5.1);
     }
-    
+
     private void createProducts() {
         Product gk_fl = new Product("GifKikker Flesje");
         gk_fl.setBeer(gifKikker);
@@ -28,28 +28,28 @@ public class ProductFactory {
         // hier voeg ik bovenstaand product toe aan de stock HashMap om de voorraad bij te houden.
         // ik zet het aantal hier op 100 stuks.
         stock.addProduct(gk_fl,100);
-        
+
         Product gk_6p = new Product("GifKikker Six-pack");
         gk_6p.setBeer(gifKikker);
         gk_6p.setPackaging(Packaging.SIXPACK);
         gk_6p.setVolume(1980);
         gk_6p.setPrice(6.99);
         stock.addProduct(gk_6p,10);
-             
+
         Product hk_cf = new Product("Heineken Champagnefles");
         hk_cf.setBeer(heineken);
         hk_cf.setPackaging(Packaging.CHAMPAGNEFLES);
         hk_cf.setVolume(750);
         hk_cf.setPrice(7.95);
         stock.addProduct(hk_cf,2);
-        
+
         Product gr_fu = new Product("Grolsch Fust");
         gr_fu.setBeer(grolsch);
         gr_fu.setPackaging(Packaging.FUST);
         gr_fu.setVolume(20_000);
         gr_fu.setPrice(59.78);
         stock.addProduct(gr_fu,5);
-        
+
         Product hj_kr = new Product("Hertog Jan Krat");
         hj_kr.setBeer(hertogJan);
         hj_kr.setPackaging(Packaging.KRAT);
@@ -57,8 +57,15 @@ public class ProductFactory {
         hj_kr.setPrice(15.49);
         stock.addProduct(hj_kr,50);
     }
-    
+
     public void printStockList() {
         stock.printStockList();
+    }
+
+    public void sellProduct(String productDescription, int amount) {
+        Product product = stock.searchProduct(productDescription);
+        if (!product.getDescription().equals("Wrong search")) {
+            stock.removeFromStock(product, amount);
+        }
     }
 }
--- GifKikkerWebshop/Stock.java
+++ GifKikkerWebshop/Stock.java
@@ -1,15 +1,19 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Stock {
     
     private HashMap<Product, Integer> stockList;
+    private ArrayList<Product> productList;
     
     public Stock() {
         stockList = new HashMap();
+        productList = new ArrayList();
     }
 
     public void addProduct(Product product, int amount) {
         stockList.put(product, amount);
+        productList.add(product);
     }
     
     public void printStockList() {
@@ -24,5 +28,25 @@ public class Stock {
     
     private void print(String print) {
         System.out.println(print);
+    }
+    
+    public void removeFromStock(Product product, int amount) {
+        int currentStock = stockList.get(product);
+        currentStock -= amount;
+        stockList.replace(product, currentStock);
+        print("Product(en) verkocht. \nVoorraad van product aangepast van " + 
+            (currentStock + amount) + " naar " + currentStock);
+        
+    }
+    
+    public Product searchProduct(String productDescription) {
+        for (Product product : productList) {
+            if (product.getDescription().equals(productDescription)) {
+                return product;
+            }
+        }
+        print("No products found that match your search :(");
+        Product p = new Product("Wrong search");
+        return p;
     }
 }
