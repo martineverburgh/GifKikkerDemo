d4fb69a770cce33cb2e878ceef2df75ddbd1c20a
Martine Verburgh <martineverburgh@gmail.com>
2018-01-31 18:44:26 +0100

    Producten geinstantieerd via de ProductFactory.

--- GifKikkerWebshop/Product.java
+++ GifKikkerWebshop/Product.java
@@ -2,9 +2,22 @@ public class Product {
     
     private String description;
     private Packaging packaging;
+    private double volume;
     private Beer beer;
     
     public Product(String decr) {
         description = decr;
+    }
+    
+    public void setPackaging(Packaging pack) {
+        packaging = pack;
+    }
+    
+    public void setVolume(double vol) {
+        volume = vol;
+    }
+    
+    public void setBeer(Beer beer) {
+        this.beer = beer;
     }
 }
--- GifKikkerWebshop/ProductFactory.java
+++ GifKikkerWebshop/ProductFactory.java
@@ -1,8 +1,10 @@
 public class ProductFactory {
     
     private Beer gifKikker, heineken, grolsch, hertogJan;
+    private Stock stock;
     
     public ProductFactory() {
+        
     }
     
     public void startWebShop() {
@@ -18,6 +20,30 @@ public class ProductFactory {
     }
     
     private void createProducts() {
+        Product gk_6p = new Product("GifKikker Six-pack");
+        gk_6p.setBeer(gifKikker);
+        gk_6p.setPackaging(Packaging.SIXPACK);
+        gk_6p.setVolume(1980);
+        // hier voeg ik bovenstaand product toe aan de stock HashMap om de voorraad bij te houden.
+        // ik zet het aantal hier op 10 stuks.
+        stock.addProduct(gk_6p,10);
         
+        Product hk_cf = new Product("Heineken Champagnefles");
+        hk_cf.setBeer(heineken);
+        hk_cf.setPackaging(Packaging.CHAMPAGNEFLES);
+        hk_cf.setVolume(750);
+        stock.addProduct(hk_cf,2);
+        
+        Product gr_fu = new Product("Grolsch Fust");
+        gr_fu.setBeer(grolsch);
+        gr_fu.setPackaging(Packaging.FUST);
+        gr_fu.setVolume(20_000);
+        stock.addProduct(gr_fu,5);
+        
+        Product hj_kr = new Product("Hertog Jan Krat");
+        hj_kr.setBeer(hertogJan);
+        hj_kr.setPackaging(Packaging.KRAT);
+        hj_kr.setVolume(7200);
+        stock.addProduct(hj_kr,50);
     }
 }
